408e819f2a44ac99ce8f85d7e062bf956ffec447
chunping <chunping@BJWS110371.(none)>
2012-11-19 09:15:59 +0800

    add gbk_to_utf8,utf8_to_unicode,unicode_to_utf8 interfaces

--- f1goal/include/f1g_lang_code.h
+++ f1goal/include/f1g_lang_code.h
@@ -12,11 +12,16 @@ enum {
 	LANG_CODE_UNKNOWN
 };
 
+
 #define URL_ESC_CHAR '%'
 
-i32_t lang_code_init();
+i32_t lang_code_init(string_t gbk_unic_map);
 
 // transfer string to code value of true type, for example: %20%ab%cd to '0x20 0xab 0xcd'
 i32_t str_decode(i8_p p_str, i32_t str_len, u8_p p_buf, i32_p data_len, u8_p code_type);
+
+i32_t gbk_to_utf8(u8_p p_gbk, i32_t gbk_len, u8_p p_utf8_buf, i32_p data_len);
+i32_t utf8_to_unicode(u8_p p_utf8, i32_t utf8_len, u8_p p_unic_buf, i32_p data_len);
+i32_t unicode_to_utf8(u16_p p_unic, i32_t unic_len, u8_p p_utf8_buf, i32_p data_len);
 
 #endif // __F1G_LANG_CODE_H
