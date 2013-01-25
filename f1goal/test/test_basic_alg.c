115b74d1bf6e1da76b3f45d79f9223e4260d12ed
chunping <chunping@BJWS110371.(none)>
2013-01-25 10:43:58 +0800

    add more test in test_basic_alg

--- f1goal/test/test_basic_alg.c
+++ f1goal/test/test_basic_alg.c
@@ -6,6 +6,23 @@
 
 #include "basic_alg.h"
 
+typedef struct _chunk_s {
+	int 	id;
+	int 	size;
+}chunk_t, *chunk_p;
+
+static void show_chunk(chunk_p p_chunk, int size)
+{
+	int i;
+
+	for (i=0; i<size; i++) {
+		if ((i+1) % 16 == 0) fprintf(stdout, "%d,%d\n", p_chunk[i].id, p_chunk[i].size);
+		else fprintf(stdout, "%d,%d ",p_chunk[i].id, p_chunk[i].size);
+	}
+
+	fprintf(stdout, "\n");
+}
+
 static void show_int_array(int *arr, int size)
 {
 	int i;
@@ -17,7 +34,7 @@ static void show_int_array(int *arr, int size)
 	fprintf(stdout, "\n");
 }
 
-static void show_string_array(char **arr, int size)
+static void show_string_array(char *arr[], int size)
 {
 	int i;
 
@@ -28,12 +45,21 @@ static void show_string_array(char **arr, int size)
 	fprintf(stdout, "\n");
 }
 
+static char *test_str_arr[] = {
+	"a", "b", "c", "d", "e",
+	"f", "h", "i", "j", "k",
+	NULL	
+};
+
+
 int main(int argc, char *argv[])
 {
 	int i;
 	int arr[10];
-	char str_arr[10][10];
+	char *str_arr[10];
 	extra_info_t extra;
+	chunk_t chunks[10];
+
 	fprintf(stdout, "sizeof(void*)=%d, sizeof(char*)=%d\n", sizeof(void*), sizeof(char*));
 
 	for (i=0; i<10; i++) arr[i] = i+1;
@@ -46,14 +72,22 @@ int main(int argc, char *argv[])
 	array_rotate((void*)arr, 10, 3, &extra);
 	show_int_array(arr, 10);
 
-	for (i=0; i<10; i++) sprintf(str_arr[i], "%d", i);
+	for (i=0; i<10; i++) str_arr[i] = test_str_arr[i];
 	show_string_array(str_arr, 10);
 
-	extra.elem_size = 10;
+	extra.elem_size = sizeof(char*);
 	extra.method = METHOD_SEQ;
 	extra.elem_type = 0;
 	array_rotate((void*)str_arr, 10, 3, &extra);
 	show_string_array(str_arr, 10);
+
+	for (i=0; i<10; i++) { chunks[i].id = i; chunks[i].size = i; }
+	show_chunk(chunks, 10);
+	extra.elem_size = sizeof(chunk_t);
+	extra.method = METHOD_SEQ;
+	extra.elem_type = 0;
+	array_rotate((void*)chunks, 10, 3, &extra);
+	show_chunk(chunks, 10);
 
 	return 0;
 }
